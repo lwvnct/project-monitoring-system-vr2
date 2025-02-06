1da7a2ba3be8de345796f2e56fe5454713060e23
lwvnct <lowievincent.canonigo@carsu.edu.ph>
2025-02-06 14:44:10 +0800

    all goods with backend

--- src/components/OriginalContract.vue
+++ src/components/OriginalContract.vue
@@ -83,7 +83,7 @@ export default {
         project_with_modified_datum: null,
       },
       projectItem: {
-        itemNo: "",
+        itemno: "",
         subDescription: "",
         quantity: null,
         unit: "",
